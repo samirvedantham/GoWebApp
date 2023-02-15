f16db69a2931b1d0fd70975a6348db154b50e460
Raghu Vir <raghuvir@Raghus-MacBook-Pro.local>
2023-02-15 11:16:13 -0600

    Added some basic functions

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const PortNumber = ":8080"
+
+func main() {
+
+	/*http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		n, err := fmt.Fprintf(w, "Hello World!")
+
+		if err != nil {
+
+			fmt.Println(err)
+		}
+
+		fmt.Println(fmt.Sprintf("Bytes Written %v", n))
+
+	})*/
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
+
+	_, _ = fmt.Printf(fmt.Sprintf("Starting application on Port: %s", PortNumber))
+
+	_ = http.ListenAndServe(PortNumber, nil) //if there's an error I don't care
+
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page!")
+
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	division, err := DivideValues(100.0, 10)
+	if err != nil {
+		fmt.Fprintf(w, fmt.Sprintf("Cannot divide by 0!"))
+	}
+	fmt.Fprintf(w, fmt.Sprintf("The result is: %f", division))
+}
+
+// About
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := AddValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2+2 is: %d", sum))
+
+}
+
+// AddValues adds 2 integers and returns the sum
+func AddValues(x, y int) int {
+	return x + y
+}
+
+func DivideValues(x, y float32) (float32, error) {
+
+	if y <= 0 {
+		err := fmt.Errorf("Cannot Divide by 0")
+		return 0.0, err
+	}
+	return x / y, nil
+}
