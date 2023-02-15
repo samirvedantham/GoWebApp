50078ba5468d1786620f6f0642541817610a0427
Raghu Vir <raghuvir@Raghus-MacBook-Pro.local>
2023-02-15 11:59:40 -0600

    Changed names to tmpl, and added basic templating

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 const PortNumber = ":8080"
@@ -32,8 +33,21 @@ func main() {
 
 }
 
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+
+	err := parsedTemplate.Execute(w, nil)
+
+	if err != nil {
+
+		fmt.Println("error parsing template")
+		return
+	}
+}
+
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the home page!")
+	RenderTemplate(w, "home.tmpl")
 
 }
 
