8ad2122d5745977b044b93f370016cb110f7059e
Raghu Vir <raghuvir@Raghus-MacBook-Pro.local>
2023-02-16 16:16:00 -0600

    Added templating

---
+++ cmd/web/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/samirvedantham/GoWebApp/pkg/handlers"
+)
+
+const PortNumber = ":8080"
+
+func main() {
+
+	/*http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		n, err := fmt.Fprintf(w, "Hello World!")
+
+		if err != nil {
+
+			fmt.Println(err)
+		}
+
+		fmt.Println(fmt.Sprintf("Bytes Written %v", n))
+
+	})*/
+
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+	//http.HandleFunc("/divide", Divide)
+
+	//_, _ = fmt.Printf(fmt.Sprintf("Starting application on Port: %s\n", PortNumber))
+
+	_ = http.ListenAndServe(PortNumber, nil) //if there's an error I don't care
+
+}
